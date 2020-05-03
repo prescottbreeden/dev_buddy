cb9a9cb1b2cfa602cca7ce4b9bc43f38e1a9a929
prescott breeden <prescott.breeden@gmail.com>
2020-05-02 19:50:21 -0700

    selectors added

--- src/components/Tasks.component.tsx
+++ src/components/Tasks.component.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { TaskType } from "../types/TaskType.type";
 import Task from "./Task.component";
+import {getTasks} from "../redux/selectors/tasks.selectors";
 
 type TasksProps = {
   tasks?: TaskType[];
@@ -42,8 +43,10 @@ const Tasks: React.FC<TasksProps> = (props) => {
   );
 };
 
-const mapStateToProps = () => {
-  const tasks: TaskType[] = [];
+const mapStateToProps = (state: any) => {
+  const tasks = getTasks(state);
+  console.log(tasks);
+
   return {
     tasks,
   };
