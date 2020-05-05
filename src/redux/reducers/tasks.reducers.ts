59824bf386c8ef6b0878f28fd3402d018fb8c42c
prescott breeden <prescott.breeden@gmail.com>
2020-05-05 09:29:43 -0700

    removed default item

--- src/redux/reducers/tasks.reducers.ts
+++ src/redux/reducers/tasks.reducers.ts
@@ -1,24 +1,7 @@
 import { SET_TASKS } from "../actions/tasks.actions";
 import { TaskType } from "../../types/TaskType.type";
-import {newId} from "../../util/misc";
 
-const initState: TaskType[] = [
-  {
-    id: newId(),
-    accumulatedTime: 258000,
-    completed: false,
-    description: "create generic workflow and approval components",
-    isActive: false,
-    name: "create generic workflow and approval components",
-    notes:
-      "These are my notes, there are many notes like them, but these are mine",
-    originalEstimate: '0',
-    priority: 1,
-    relatedFeature: "Display of Needs Assessment Results",
-    startedDate: new Date(2020, 4, 1, 12),
-  }
-
-];
+const initState: TaskType[] = [];
 
 export const tasksReducer = (tasks = initState, action: any) => {
   switch (action.type) {
